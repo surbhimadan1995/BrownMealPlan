217b0dbdfe4d85f542b4871a0b7b412abfcbec7c
Tushar Bhargava <tushar1995@gmail.com>
2015-02-07 16:06:46 -0500

    Java prototype

--- my_config.js
+++ my_config.js
@@ -1,8 +1,8 @@
 pjs.addSuite({
     // single URL or array
-    url: 'http://brownrec.com/aquatics/hours',
+    url: 'www.brown.edu/Student_Services/Food_Services/eateries/refectory_menu.php',
     // single function or array, evaluated in the client
     scraper: function() {
-        return $('header#site-header').text();
+        return $('').text();
     }
 });
